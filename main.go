84e7f131db0b60d9395398f6aa0ff72d02de2d7a
Emmanuel Bastien <os@ebastien.name>
2019-11-23 20:58:01 +0100

    Concurrency limit.

--- main.go
+++ main.go
@@ -8,42 +8,59 @@ import (
 	"github.com/ebastien/hello-go/solver"
 )
 
-type HandlerState struct {
-	model solver.Model
+type ServerState struct {
+	model   solver.Model
+	workers chan struct{}
 }
 
-func (s *HandlerState) solveHandler(w http.ResponseWriter, r *http.Request) {
+func initModel(m *solver.Model) {
+	m.Init("var int: age; constraint age >= 1; constraint age <= 2; solve satisfy;")
 
+	fmt.Printf("Compile model: %s\n", m.Minizinc())
+
+	if err := m.Compile(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func solveModel(m solver.Model) error {
 	type Solution struct {
 		Age int
 	}
 	var solution Solution
 
-	fmt.Printf("Solve model: %s\n", s.model.Flatzinc())
+	fmt.Printf("Solve model: %s\n", m.Flatzinc())
 
-	status, err := s.model.Solve(&solution)
-	if err != nil {
-		log.Fatal(err)
+	status, err := m.Solve(&solution)
+	if err == nil {
+		fmt.Printf("solution = %#v\n", solution)
+		fmt.Printf("status = %v\n", status)
 	}
+	return err
+}
 
-	fmt.Printf("solution = %#v\n", solution)
-	fmt.Printf("status = %v\n", status)
+func (s *ServerState) solveHandler(w http.ResponseWriter, r *http.Request) {
+	s.workers <- struct{}{}
+	defer func() { <-s.workers }()
 
-	w.WriteHeader(http.StatusOK)
+	// time.Sleep(5 * time.Second)
+
+	err := solveModel(s.model)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
 }
 
 func main() {
 	fmt.Println("Hello, World!")
 
-	var state HandlerState
-
-	state.model.Init("var int: age; constraint age >= 1; constraint age <= 2; solve satisfy;")
-
-	fmt.Printf("Compile model: %s\n", state.model.Minizinc())
-
-	if err := state.model.Compile(); err != nil {
-		log.Fatal(err)
+	state := ServerState{
+		workers: make(chan struct{}, 3),
 	}
+
+	initModel(&state.model)
 
 	http.HandleFunc("/", state.solveHandler)
 
